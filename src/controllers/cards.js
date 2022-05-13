54531c7907e26a98e9107f5937b6e266f9279b53
Kruglikov <yandexart@yandex.ru>
2022-05-13 11:58:52 +0300

    sprint 13

---
+++ src/controllers/cards.js
@@ -0,0 +1,20 @@
+const Card = require('../models/card');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.deleteCard = (req, res) => {
+  Card.deleteOne({ _id: req.params.cardId })
+    .then((result) => res.send({ data: result }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  Card.create({ name, link, owner: req.user._id })
+    .then((item) => res.send({ data: item }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
